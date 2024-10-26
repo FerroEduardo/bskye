97a4927997650631660de5a99949b5d09b88a719
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2024-10-25 23:51:13 -0300

    fix: problem when replacing special html characters

--- src/util.ts
+++ src/util.ts
@@ -28,7 +28,7 @@ export function generateOembedUrl(host: string, link: string, title: string, aut
 export const metricsFormatter = Intl.NumberFormat(undefined, { notation: 'compact', maximumFractionDigits: 1 });
 
 export function escapeHtml(text: string) {
-  return text.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
+  return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
 }
 
 export function getUserDisplayString(displayName: string | undefined, handle: string): string {
