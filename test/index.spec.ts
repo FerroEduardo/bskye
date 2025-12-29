627b8f4c6ad084bc87520b489eca0a68a1aafe5a
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2025-12-29 10:32:45 -0300

    test: add index test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,10 +1,12 @@
-import {} from 'cloudflare:test';
+import { env } from 'cloudflare:test';
 import { describe, expect, it } from 'vitest';
+import app from '../src';
 
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+describe('Example', () => {
+  it('Should return 200 response', async () => {
+    const res = await app.request('/', {}, env);
 
-describe('worker', () => {
-  it('success', async () => {
-    expect('hello').toBe('hello');
+    expect(res.status).toBe(302);
+    expect(res.headers.get('Location')).toBe('https://github.com/FerroEduardo/bskye');
   });
 });
--- vitest.config.mts
+++ vitest.config.mts
@@ -2,6 +2,7 @@ import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
 
 export default defineWorkersConfig({
   test: {
+    globals: true,
     poolOptions: {
       workers: {
         wrangler: { configPath: './wrangler.toml' }
